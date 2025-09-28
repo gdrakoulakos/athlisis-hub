8ffe9462283ee4beee069e16a82f5177b41c0cf0
George <georgediopan@gmail.com>
2025-09-29 00:10:14 +0300

    Add images and mock data for bookings; enhance Upcomings component with dynamic rendering

--- src/app/components/Upcomings/Upcomings.js
+++ src/app/components/Upcomings/Upcomings.js
@@ -1,18 +1,35 @@
+import Image from "next/image";
 import styles from "./Upcomings.module.css";
+import bookings from "@/mockdata/bookings.json";
 
 export default function Upcomings() {
+  console.log("bookings", bookings);
+
   return (
-    <div className={styles.upcomingsSection}>
-      <h3>Upcomings</h3>
-      <div className={styles.upcomingsContainer}>
-        <div className={styles.image}>Image</div>
-        <div className={styles.infoContainer}>
-          <div className={styles.date}>Date</div>
-          <div className={styles.Name}>Name</div>
-          <div className={styles.type}>Type</div>
-          <div className={styles.more}>more...</div>
-        </div>
-      </div>
-    </div>
+    <>
+      <h2 className={styles.title}>Upcomings</h2>
+      <section className={styles.upcomingsSection}>
+        {bookings.map((booking, index) => (
+          <article key={index} className={styles.upcomingsContainer}>
+            <figure className={styles.imageContainer}>
+              <Image
+                src={`/images/${booking.type}.png`}
+                alt="Tennis Icon"
+                width={60}
+                height={60}
+                loading="lazy"
+                className={styles.image}
+              />
+            </figure>
+            <div className={styles.infoContainer}>
+              <time className={styles.date}>{booking.date}</time>
+              <div className={styles.Name}>{booking.name}</div>
+              <div className={styles.type}>{booking.type}</div>
+              <button className={styles.more}>More ...</button>
+            </div>
+          </article>
+        ))}
+      </section>
+    </>
   );
 }
