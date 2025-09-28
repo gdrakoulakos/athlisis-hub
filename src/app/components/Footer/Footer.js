c40b0ea04a5a0fdc96f3fd9d320735a7d4338b0c
George <georgediopan@gmail.com>
2025-09-28 21:22:21 +0300

    Add Footer component and styles; integrate into layout

---
+++ src/app/components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import styles from "./Footer.module.css";
+
+export default function Footer() {
+  const currentYear = new Date().getFullYear();
+  return (
+    <div className={styles.footerSection}>
+      <p>&copy; George Drakoulakos {currentYear}</p>
+    </div>
+  );
+}
