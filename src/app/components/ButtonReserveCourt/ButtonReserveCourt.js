39199a7035245be0ac8f3863676a42e25ab89d4c
George <georgediopan@gmail.com>
2025-10-01 02:32:18 +0300

    Add ButtonReserveCourt component and integrate into Home page; update Upcomings styles and Header option styles

---
+++ src/app/components/ButtonReserveCourt/ButtonReserveCourt.js
@@ -0,0 +1,5 @@
+import styles from "./ButtonReserveCourt.module.css";
+
+export default function ButtonReserveCourt() {
+  return <button className={styles.reserveButton}>Reserve a Court</button>;
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import styles from "./page.module.css";
 import Upcomings from "./components/Upcomings/Upcomings";
+import ButtonReserveCourt from "./components/ButtonReserveCourt/ButtonReserveCourt";
 
 export default function Home() {
   return (
-  <div className={styles.homePageSection}>
-    <Upcomings />
-  </div>
-)
+    <div className={styles.homePageSection}>
+      <Upcomings />
+      <ButtonReserveCourt />
+    </div>
+  );
 }
