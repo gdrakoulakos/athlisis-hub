5bc098cad438938b2c8b4d900f34982523ea76f3
George <georgediopan@gmail.com>
2025-10-07 11:34:32 +0300

    Add ManageBookings component and associated CSS module

---
+++ src/app/manage-bookings/page.js
@@ -0,0 +1,9 @@
+import styles from "./page.module.css";
+
+export default function ManageBookings() {
+  return (
+    <div className={styles.manageBookingsContainer}>
+      <h2>Manage Bookings</h2>
+    </div>
+  );
+}
