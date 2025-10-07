ccb000267f5e64013696a08b6fb0377245d61ee0
George <georgediopan@gmail.com>
2025-10-07 20:22:25 +0300

    Integrate booking API query using useGetBookingsQuery in Home component

--- src/app/page.js
+++ src/app/page.js
@@ -3,9 +3,13 @@ import { useEffect, useState } from "react";
 import styles from "./page.module.css";
 import RequestsOverview from "./components/RequestsOverview/RequestsOverview";
 import ButtonReserveCourt from "./components/ButtonReserveCourt/ButtonReserveCourt";
+import { useGetBookingsQuery } from "@/store/bookingApi";
 
 export default function Home() {
   const [bookings, setBookings] = useState([]);
+  const { data, isLoading, error } = useGetBookingsQuery();
+
+  console.log("data", data);
 
   useEffect(() => {
     async function fetchData() {
