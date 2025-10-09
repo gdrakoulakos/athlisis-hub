152c1d7187f30a884d569cb584cb64ee34014f32
George <georgediopan@gmail.com>
2025-10-09 15:01:30 +0300

    Refactor ManageBookings component to render booking details dynamically and integrate ImageBookingType for improved visuals

--- src/app/manage-bookings/page.js
+++ src/app/manage-bookings/page.js
@@ -1,58 +1,59 @@
 "use client";
 import { useSelector } from "react-redux";
 import styles from "./page.module.css";
+import ImageBookingType from "../components/ImageBookingType/ImageBookingType";
 
 export default function ManageBookings() {
   const bookings = useSelector((state) => state.bookings.list);
 
   console.log("bookings", bookings);
 
-  const tableHeaderOptions = ["Date", "Time", "Duration", "Name"];
-
   return (
-    <div className={styles.manageBookingsContainer}>
+    <div className={styles.manageBookingsSection}>
       <h2>Manage Bookings</h2>
-      <div className={styles.cardContainer}>
-        <div className={styles.cardHeader}>
-          <div className={styles.headerLeftInfo}>
-            <div className={styles.image}>☺</div>
-            <div className={styles.headerLeftItems}>
-              <div className={styles.headerName}>George Drakoulakos</div>
-              <div className={styles.headerDateTimeContainer}>
-                <div className={styles.headerDate}>20-10-2025</div>
-                <div className={styles.headerTime}>10:00</div>
+      {bookings.map((booking) => (
+        <div key={booking.id} className={styles.cardContainer}>
+          <div className={styles.cardHeader}>
+            <div className={styles.headerLeftInfo}>
+              <ImageBookingType bookingType={booking.type} size={40}/>
+              <div className={styles.headerLeftItems}>
+                <h4 className={styles.headerName}>{booking.name}</h4>
+                <div className={styles.headerDateTimeContainer}>
+                  <div className={styles.headerDate}>{booking.date}</div>
+                  <div className={styles.headerTime}>{booking.time}</div>
+                </div>
+              </div>
+            </div>
+            <div className={styles.headerRightInfo}>
+              <div className={styles.headerStatusInfo}>{booking.status} ➢ </div>
+            </div>
+          </div>
+          <div>
+            <div className={styles.detailsContainer}>
+              <div className={styles.type}>
+                <div>Type: </div>
+                <div>{booking.type}</div>
+              </div>
+              <div className={styles.persons}>
+                <div>Persons:</div>
+                <div>{booking.persons}</div>
+              </div>
+              <div className={styles.duration}>
+                <div>Duration:</div>
+                <div>{booking.duration}</div>
+              </div>
+              <div className={styles.phone}>
+                <div>Phone:</div>
+                <div>{booking.phone}</div>
+              </div>
+              <div className={styles.court}>
+                <div>Court:</div>
+                <div>{booking.court}</div>
               </div>
             </div>
           </div>
-          <div className={styles.headerRightInfo}>
-            <div className={styles.headerStatusInfo}>Pending ➢ </div>
-          </div>
         </div>
-        <div>
-          <div className={styles.detailsContainer}>
-            <div className={styles.type}>
-              <div>Type: </div>
-              <div>Friendly</div>
-            </div>
-            <div className={styles.persons}>
-              <div>Persons:</div>
-              <div>4</div>
-            </div>
-            <div className={styles.duration}>
-              <div>Duration:</div>
-              <div>2 hours</div>
-            </div>
-            <div className={styles.phone}>
-              <div>Phone:</div>
-              <div>6985225566</div>
-            </div>
-            <div className={styles.court}>
-              <div>Court:</div>
-              <div>2</div>
-            </div>
-          </div>
-        </div>
-      </div>
+      ))}
     </div>
   );
 }
