06e471697b9207b6cfa0d3f3adc25b8ad575ff47
George <georgediopan@gmail.com>
2025-10-27 01:26:31 +0200

    Install the library react-hook-form and implement it to bookings to prevent invalid inputs

--- src/app/booking/page.js
+++ src/app/booking/page.js
@@ -3,6 +3,7 @@ import Calendar from "react-calendar";
 import styles from "./page.module.css";
 import "react-calendar/dist/Calendar.css";
 import { useState } from "react";
+import { useForm } from "react-hook-form";
 import ConfirmationPopUp from "../components/popUps/ConfirmationPopUp/ConfirmationPopUp";
 import ResultPopUp from "../components/popUps/ResultPopUp/ResultPopUp";
 import { displayConfirmationPopUp } from "@/redux/features/popUps/confirmationPopUpSlice";
@@ -11,6 +12,11 @@ import { useDispatch } from "react-redux";
 
 export default function Booking() {
   const dispatch = useDispatch();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
   const today = new Date();
   const formattedToday = `${today.getFullYear()}-${String(
@@ -100,143 +106,182 @@ export default function Booking() {
     }
   };
 
+  const onSubmit = () => {
+    dispatch(displayConfirmationPopUp());
+  };
+
   return (
     <>
       {bookingData && (
         <div className={styles.bookingBlock}>
           <h1>Booking</h1>
-          <div className={styles.nameContainer}>
-            <h3>Name:</h3>
-            <input
-              className={styles.nameInput}
-              type="text"
-              placeholder="Your name..."
-              name="name"
-              value={bookingData.name}
-              onChange={handleChange}
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className={styles.nameContainer}>
+              <h3>Name:</h3>
+              <input
+                className={styles.nameInput}
+                type="text"
+                placeholder="Your name..."
+                {...register("name", {
+                  required: "Name is required",
+                  minLength: {
+                    value: 2,
+                    message: "Name must have more than 2 letters",
+                  },
+                })}
+                name="name"
+                value={bookingData.name}
+                onChange={handleChange}
+              />
+              {errors.name && (
+                <span className="error-message">{errors.name.message}</span>
+              )}
+            </div>
+            <div className={styles.phoneContainer}>
+              <h3>Phone Number:</h3>
+              <input
+                className={styles.phoneInput}
+                type="tel"
+                inputMode="numeric"
+                {...register("phone", {
+                  required: "Phone number is required",
+                  pattern: {
+                    value: /^(?:69\d{8}|210\d{7})$/,
+                    message:
+                      "Phone number must be 10 digits and starts with 69... or 210...",
+                  },
+                })}
+                placeholder="Your phone number..."
+                name="phone"
+                value={bookingData.phone}
+                onChange={handleChange}
+                maxLength={10}
+                required
+              />
+              {errors.phone && (
+                <span className="error-message">{errors.phone.message}</span>
+              )}
+            </div>
+            <div className={styles.bookingTypeContainer}>
+              <h3>Booking Type:</h3>
+              <select
+                className={styles.bookingTypeOptionsContainer}
+                name="type"
+                id="booking-type-select"
+                value={bookingData.type}
+                onChange={handleChange}
+              >
+                {bookingTypeOptions.map((option, index) => (
+                  <option key={index} value={option}>
+                    {option}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div className={styles.personsContainer}>
+              <h3>Persons:</h3>
+              <input
+                className={styles.persons}
+                type="number"
+                inputMode="numeric"
+                placeholder="Number of persons..."
+                name="persons"
+                {...register("persons", {
+                  required: "Number of persons is required",
+                  max: {
+                    value: 50,
+                    message: "Number of persons cannot be more than 50",
+                  },
+                })}
+                value={bookingData.persons}
+                onChange={handleChange}
+              />
+              {errors.persons && (
+                <span className="error-message">{errors.persons.message}</span>
+              )}
+            </div>
+            <div className={styles.dateSelectionContainer}>
+              <h3>Date:</h3>
+              <Calendar
+                onChange={(date) =>
+                  setBookingData((prev) => {
+                    const year = date.getFullYear();
+                    const month = String(date.getMonth() + 1).padStart(2, "0");
+                    const day = String(date.getDate()).padStart(2, "0");
+                    return {
+                      ...prev,
+                      date: `${year}-${month}-${day}`,
+                    };
+                  })
+                }
+                value={bookingData.date}
+                locale="en-US"
+                calendarType="iso8601"
+                minDate={new Date()}
+              />
+            </div>
+            <div className={styles.timeContainer}>
+              <h3>Time:</h3>
+              <select
+                className={styles.timeOptionsContainer}
+                name="time"
+                id="time-select"
+                value={bookingData.time}
+                onChange={handleChange}
+              >
+                {timeOptions.map((time, index) => (
+                  <option key={index} value={time.value}>
+                    {time.label}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div className={styles.durationSelectionContainer}>
+              <h3>Duration:</h3>
+              <select
+                className={styles.durationOptionsContainer}
+                name="duration"
+                id="duration-select"
+                value={bookingData.duration}
+                onChange={handleChange}
+              >
+                {durationOptions.map((option, index) => (
+                  <option key={index} value={option.value}>
+                    {option.label}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div className={styles.courtSelectionContainer}>
+              <h3>Court:</h3>
+              <select
+                className={styles.courtOptionsContainer}
+                name="court"
+                id="court-select"
+                value={bookingData.court}
+                onChange={handleChange}
+              >
+                {courtOptions.map((option, index) => (
+                  <option key={index} value={option.value}>
+                    {option.label}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <button className={styles.submitButton} type="submit">
+              Submit Booking
+            </button>
+            <ConfirmationPopUp
+              bookingData={bookingData}
+              addBooking={addBooking}
+              message={"Please confirm your booking"}
+              action={bookingData.action}
             />
-          </div>
-          <div className={styles.phoneContainer}>
-            <h3>Phone Number:</h3>
-            <input
-              className={styles.phoneInput}
-              type="number"
-              placeholder="Your phone number..."
-              name="phone"
-              value={bookingData.phone}
-              onChange={handleChange}
+            <ResultPopUp
+              message={"Booking added successfully"}
+              action={bookingData.action}
             />
-          </div>
-          <div className={styles.bookingTypeContainer}>
-            <h3>Booking Type:</h3>
-            <select
-              className={styles.bookingTypeOptionsContainer}
-              name="type"
-              id="booking-type-select"
-              value={bookingData.type}
-              onChange={handleChange}
-            >
-              {bookingTypeOptions.map((option, index) => (
-                <option key={index} value={option}>
-                  {option}
-                </option>
-              ))}
-            </select>
-          </div>
-          <div className={styles.personsContainer}>
-            <h3>Persons:</h3>
-            <input
-              className={styles.nameInput}
-              type="number"
-              placeholder="Number of persons..."
-              name="persons"
-              value={bookingData.persons}
-              onChange={handleChange}
-            />
-          </div>
-          <div className={styles.dateSelectionContainer}>
-            <h3>Date:</h3>
-            <Calendar
-              onChange={(date) =>
-                setBookingData((prev) => {
-                  const year = date.getFullYear();
-                  const month = String(date.getMonth() + 1).padStart(2, "0");
-                  const day = String(date.getDate()).padStart(2, "0");
-                  return {
-                    ...prev,
-                    date: `${year}-${month}-${day}`,
-                  };
-                })
-              }
-              value={bookingData.date}
-              locale="en-US"
-              calendarType="iso8601"
-            />
-          </div>
-          <div className={styles.timeContainer}>
-            <h3>Time:</h3>
-            <select
-              className={styles.timeOptionsContainer}
-              name="time"
-              id="time-select"
-              value={bookingData.time}
-              onChange={handleChange}
-            >
-              {timeOptions.map((time, index) => (
-                <option key={index} value={time.value}>
-                  {time.label}
-                </option>
-              ))}
-            </select>
-          </div>
-          <div className={styles.durationSelectionContainer}>
-            <h3>Duration:</h3>
-            <select
-              className={styles.durationOptionsContainer}
-              name="duration"
-              id="duration-select"
-              value={bookingData.duration}
-              onChange={handleChange}
-            >
-              {durationOptions.map((option, index) => (
-                <option key={index} value={option.value}>
-                  {option.label}
-                </option>
-              ))}
-            </select>
-          </div>
-          <div className={styles.courtSelectionContainer}>
-            <h3>Court:</h3>
-            <select
-              className={styles.courtOptionsContainer}
-              name="court"
-              id="court-select"
-              value={bookingData.court}
-              onChange={handleChange}
-            >
-              {courtOptions.map((option, index) => (
-                <option key={index} value={option.value}>
-                  {option.label}
-                </option>
-              ))}
-            </select>
-          </div>
-          <button
-            className={styles.submitButton}
-            onClick={() => dispatch(displayConfirmationPopUp())}
-          >
-            Submit Booking
-          </button>
-          <ConfirmationPopUp
-            bookingData={bookingData}
-            addBooking={addBooking}
-            message={"Please confirm your booking"}
-            action={bookingData.action}
-          />
-          <ResultPopUp
-            message={"Booking added successfully"}
-            action={bookingData.action}
-          />
+          </form>
         </div>
       )}
     </>
