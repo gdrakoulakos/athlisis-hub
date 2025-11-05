297b9d10c41d02e008f19db9a1c85e266baee597
george <g.drakoulakos@liquid.gr>
2025-11-05 16:07:19 +0200

    Add additional time options to TimePicker and refine CSS styles for better layout

--- src/app/components/TimePicker/TimePicker.js
+++ src/app/components/TimePicker/TimePicker.js
@@ -13,6 +13,13 @@ export default function TimePicker({ handleChange }) {
     { label: "13:00", value: "13:00:00" },
     { label: "14:00", value: "14:00:00" },
     { label: "15:00", value: "15:00:00" },
+    { label: "16:00", value: "16:00:00" },
+    { label: "17:00", value: "17:00:00" },
+    { label: "18:00", value: "18:00:00" },
+    { label: "19:00", value: "19:00:00" },
+    { label: "20:00", value: "20:00:00" },
+    { label: "21:00", value: "21:00:00" },
+    { label: "22:00", value: "22:00:00" },
   ];
 
   const handleTimePickerClick = (value) => {
