27fdff88a5c07e54592b52d2f9b9368fc1e8b6bd
george <g.drakoulakos@liquid.gr>
2025-11-19 14:51:24 +0200

    Add dynamic export to page component

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,6 @@
 "use client";
+export const dynamic = "force-dynamic";
+
 import styles from "./page.module.css";
 import RequestsOverview from "./components/RequestsOverview/RequestsOverview";
 import ButtonReserveCourt from "./components/ButtonReserveCourt/ButtonReserveCourt";
